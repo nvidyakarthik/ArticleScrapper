107369233bf0381c7f3d10d5ff46c623807ccadb
nvidya.karthik <nvidya.karthik@gmail.com>
2018-06-06 18:27:54 -0700

    working scraper

--- server.js
+++ server.js
@@ -29,6 +29,48 @@ app.use(express.static("public"));
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/articleScrapper");
 
+// A GET route for scraping the echoJS website
+app.get("/scrape", function(req, res) {
+    // First, we grab the body of the html with request
+    axios.get("https://www.livescience.com/environment?type=article").then(function(response) {
+      // Then, we load that into cheerio and save it to $ for a shorthand selector
+      var $ = cheerio.load(response.data);
+     
+      // Now, we grab every h2 within an article tag, and do the following:
+      $("li").each(function(i, element) {
+        // Save an empty result object
+        var result = {};
+  
+        // Add the text and href of every link, and save them as properties of the result object
+        result.link = $(this)
+        .children("div").children("h2").children("a")
+        .attr("href");  
+        result.headline = $(this)
+          .children("div").children("h2").children("a")
+          .text();
+        result.summary = $(this)
+          .children("div").children("p")
+          .text();
+         
+  
+        // Create a new Article using the `result` object built from scraping
+        db.Article.create(result)
+          .then(function(dbArticle) {
+            // View the added result in the console
+            console.log(dbArticle);
+            
+          })
+          .catch(function(err) {
+            // If an error occurred, send it to the client
+            return res.json(err);
+          });
+      });
+  
+      // If we were able to successfully scrape and save an Article, send a message to the client
+      //res.send("Scrape Complete");
+    });
+  });
+
 // Start the server
 app.listen(PORT, function() {
     console.log("App running on port " + PORT + "!");
