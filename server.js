1d4c7c50cc06022a927647a8e119573b53eba5a6
nvidya.karthik <nvidya.karthik@gmail.com>
2018-06-07 14:46:37 -0700

    mongo server connection check added

--- server.js
+++ server.js
@@ -3,15 +3,6 @@ var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
-var axios = require("axios");
-var cheerio = require("cheerio");
-
-// Require all models
-var db = require("./models");
-
 var PORT = 3000;
 
 // Initialize Express
@@ -23,6 +14,7 @@ var app = express();
 app.use(logger("dev"));
 // Use body-parser for handling form submissions
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
@@ -34,7 +26,9 @@ app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/articleScrapper");
-
+mongoose.connection.on('error', function() {
+    console.error('MongoDB Connection Error. Make sure MongoDB is running.');
+});
 require("./routes/api-routes.js")(app);
 
 // Start the server
