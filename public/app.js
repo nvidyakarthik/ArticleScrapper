8ac4b1aaea6844108c85ef17a63c436d4489a176
nvidya.karthik <nvidya.karthik@gmail.com>
2018-06-08 16:17:01 -0700

    modal box ready for saving notes

--- public/app.js
+++ public/app.js
@@ -42,11 +42,10 @@ $(document).on("click", "#delArticle", function() {
 
 //This is for modal showing message id
 $(document).on('show.bs.modal', '#exampleModal', function(event) {
-  var button = $(event.relatedTarget) // Button that triggered the modal
-  var recipient = button.data('id') // Extract info from data-* attributes
+  var button = $(event.relatedTarget); // Button that triggered the modal
+  var articleId = button.data('id'); // Extract info from data-* attributes
   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
   var modal = $(this)
-  modal.find('.modal-title').text('New message to ' + recipient)
-  modal.find('.modal-body input').val(recipient)
+  modal.find('.modal-title').text('Notes for Article : ' + articleId);
 })
