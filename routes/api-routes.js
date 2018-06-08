7864b89ab3575caf44a9d76d1a4f03077190f12a
nvidya.karthik <nvidya.karthik@gmail.com>
2018-06-08 13:36:24 -0700

    added article saved routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,49 +1,107 @@
 var db = require("../models");
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
 module.exports = function (app) {
     app.get("/", function (req, res) {
-        res.render("index");
+        db.Article.find({})
+        .then(function(dbArticles) {
+            // If we were able to successfully find Articles, send them back to the client
+           var obj={
+               dbArticle:dbArticles
+           }
+            res.render("index",obj);
+          })
+          .catch(function(err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+          });
+        
     });
 
+    app.get("/savedArticles", function (req, res) {
+        res.render("saved");
+    });    
+
     // A GET route for scraping the echoJS website
-    app.get("/scrape", function (req, res) {
+    app.get("/scrapeArticles", function (req, res) {
         // First, we grab the body of the html with request
         axios.get("https://www.livescience.com/environment?type=article").then(function (response) {
             // Then, we load that into cheerio and save it to $ for a shorthand selector
             var $ = cheerio.load(response.data);
 
             // Now, we grab every h2 within an article tag, and do the following:
-            $("li").each(function (i, element) {
+            $(".mod-copy").each(function (i, element) {
                 // Save an empty result object
                 var result = {};
-
+                var parentDiv=$(this).parent();    
                 // Add the text and href of every link, and save them as properties of the result object
-                result.link = $(this)
-                    .children("div").children("h2").children("a")
+                result.link = "https://www.livescience.com"+parentDiv
+                    .children("h2").children("a")
                     .attr("href");
-                result.headline = $(this)
-                    .children("div").children("h2").children("a")
-                    .text();
-                result.summary = $(this)
-                    .children("div").children("p")
-                    .text();
-
+                result.headline = parentDiv
+                    .children("h2").children("a")
+                    .text().trim();
+                result.summary = parentDiv
+                    .children("p")
+                    .text().trim();
+                
 
                 // Create a new Article using the `result` object built from scraping
                 db.Article.create(result)
                     .then(function (dbArticle) {
                         // View the added result in the console
-                        console.log(dbArticle);
+                       console.log(dbArticle);
+                       
 
                     })
                     .catch(function (err) {
                         // If an error occurred, send it to the client
-                        return res.json(err);
+                        //return res.json(err);
+                        console.log(err);
                     });
             });
-
-            // If we were able to successfully scrape and save an Article, send a message to the client
-            //res.send("Scrape Complete");
+            
+            
         });
+        res.redirect("/");
+        
+    });
+
+    app.get("/articleSaved/:id", function (req, res) {
+        db.Article.findOneAndUpdate({ _id: req.params.id }, { isSaved:true }, { new: true })
+        .then(function(dbArticle) {
+            // If we were able to successfully update an Article, send it back to the client
+           // res.json(dbArticle);
+           console.log("updated");
+           console.log(dbArticle);
+           
+          })
+          .catch(function(err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+          });
+        
+    });
+    app.get("/mySavedArticles", function (req, res) {
+        console.log("inside");
+       db.Article.find({isSaved:true})
+         .then(function(dbresults) {
+            // If we were able to successfully find Articles, send them back to the client
+           console.log(dbresults);
+           console.log("inside");
+            var articleObject={
+               savedArticleList:dbresults
+           }
+            res.render("saved",articleObject);
+          })
+          .catch(function(err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+          }); 
+        
     });
 
 };
