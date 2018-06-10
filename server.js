7e1903a8b568fa5b4da310b872cefd80bd46cf30
nvidya.karthik <nvidya.karthik@gmail.com>
2018-06-10 00:18:54 -0700

    mongo connection for heroku

--- server.js
+++ server.js
@@ -25,7 +25,14 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/articleScrapper");
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/articleScrapper";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
 mongoose.connection.on('error', function() {
     console.error('MongoDB Connection Error. Make sure MongoDB is running.');
 });
